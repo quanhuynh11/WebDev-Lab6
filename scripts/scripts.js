362e23547699209b7dda062ed8bf870699df58da
quanhuynh11 <quan.m.huynh@edu.sait.ca>
2023-11-28 17:46:13 -0700

    completed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,8 +10,8 @@ You are encouraged to use the provided naming convention for ease of review.
 /* create variables to hold the values for modelName and duration */
 
 // INSERT YOUR CODE HERE
-modelName = document.getElementById("model-text"); 
-console.log(modelName);
+let modelNameValue = "XYZ";
+let duration = 0;
 
 
 
@@ -27,17 +27,21 @@ console.log(modelName);
 */
 
 // INSERT YOUR CODE HERE
-function recalculate() {
+function recalculate(modelName, duration) {
     let constLabel = document.getElementById("calculated-cost");
-
-    // if(modelName == "Model XYZ") {
-
-    // }
+    let cost;
+    
+    // when modelName is "Model XYZ"
+    if(modelName == "XYZ") {
+        cost = 100 * duration;
+    }
+    // when modelName is "Model CPRG"
+    else if(modelName == "CPRG") {
+        cost = 213 * duration;
+    }
+    // Set the new innerText to the cost
+    constLabel.innerText = cost;
 }
-
-
-
-
 
 /****************** model button logic ******************/
 
@@ -49,14 +53,26 @@ function recalculate() {
     - if modelName is currently "CPRG", change the value of modelName to "XYZ", and change the innerHTML of the model-text span element to "Model XYZ"
     - then, recalculate() the total cost.
 - finally, uncomment the following line of JavaScript to have this function run automatically whenever the pseudo-button is clicked: */
-    // modelButton.addEventListener("click", changeModel);
+let modelButton = document.getElementById("model-button");
+modelButton.addEventListener("click", changeModel);
+
 
 // INSERT YOUR CODE HERE
+function changeModel() {
+    let modelText = document.getElementById("model-text");
 
+    if(modelNameValue == "XYZ") {
+        modelText.innerText = "Model CPRG";
+        modelNameValue = "CPRG";
+    }
+    else if(modelNameValue == "CPRG") {
+        modelText.innerText = "Model XYZ";
+        modelNameValue = "XYZ";
+    }
 
+    recalculate(modelNameValue, duration);
 
-
-
+}
 
 
 /****************** duration button logic ******************/
@@ -71,6 +87,16 @@ function recalculate() {
 */
 
 // INSERT YOUR CODE HERE
+let durationButton = document.getElementById("duration-button");
+durationButton.addEventListener("click", changeDuration);
 
+function changeDuration() {
+    let durationText = document.getElementById("duration-text");
+    
+    let newDuration = prompt("Enter a Duration");
+    duration = newDuration
+    durationText.innerText = newDuration;
 
+    recalculate(modelNameValue, duration);
+}
 
